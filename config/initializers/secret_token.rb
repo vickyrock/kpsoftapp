d642835308c66056b493ee7e539c451375f8d911
Kamlesh <kamlesh@kamlesh-To-be-filled-by-O-E-M.(none)>
2014-10-31 14:48:27 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KpsoftApp::Application.config.secret_key_base = '5399a537e572987bc6aa6dd127484d2b21f0df46306968210d2ac6763fd7378fef32e94344f7d56ca8e3134e75ba00b328975765cd35e09337b5ddaa76e1f9f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KpsoftApp::Application.config.session_store :cookie_store, key: '_kpsoft_app_session'
